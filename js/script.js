9bc5b5c9e3c571f3e81c5f04f8bfc6263b6be8a8
MargaritaVin <ritasergienko@rambler.ru>
2022-08-02 13:15:04 +0300

    added function findShort in script.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,13 @@ function sumTo(n) {
 };
 console.log(sumTo(4)); 
 console.log(sumTo(3)); 
+
+const sentance = "Lorem ipsum dolor sit amet";
+function findShort(string) {
+    let array = string.split(" ");
+    return array.reduce((elem, index) => index.length < elem.length ? index : elem, array[0]);
+};
+console.log(findShort(sentance));
+console.log(findShort("Hello world")); 
+console.log(findShort("Hi")); 
+console.log(findShort("She is David's sister")); 
